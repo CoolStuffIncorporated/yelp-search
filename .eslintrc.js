6a1f894bd4e0a5ce8070c2dc45fbb584a7388496
Charles <charles.hufnagel@protonmail.com>
2018-05-29 13:23:15 -0400

    Git init

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'space-before-function-paren': 'off',
+      'func-names': 'off',
+      'no-shadow': 'off',
+      'camelcase': 'off',
+      'global-require': 'off',
+      'object-shorthand': 'off',
+    }
+  };
