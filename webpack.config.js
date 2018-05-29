e19f10ec6d3d5ebf2f76b65527ff9f27fc351837
Charles <charles.hufnagel@protonmail.com>
2018-05-29 13:39:37 -0400

    Add code examples and basic directory structure

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  }
+};
