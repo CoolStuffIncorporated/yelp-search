bf46cfcd7424564fcc5561b459529573240a807b
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-05-30 15:42:12 -0400

    fix changes to chalk

--- server/index.js
+++ server/index.js
@@ -21,12 +21,11 @@ const searchRequest = {
 };
 
 // chalk logging
-const { log } = console.log;
+const log = console.log;
 const succ = chalk.bold.green.bgWhite; // use to log success
 const errc = chalk.bold.red.bgBlack; // UH OH
 const warc = chalk.underline.orange; // log concerning but non-breaking
 const infoc = chalk.blue.bgBlack; // log general information
-
 
 const app = express();
 
