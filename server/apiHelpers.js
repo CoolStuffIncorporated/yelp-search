920bc121d1db575886ce3f965cc80787b622dc6b
Alon Bibring <alon.bibring@gmail.com>
2018-05-30 17:13:35 -0400

    updated apiHelpers

--- server/apiHelpers.js
+++ server/apiHelpers.js
@@ -1,36 +1,31 @@
 const axios = require('axios');
+const request = require('request');
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { API_TOKEN } = require('./env/config.js').API_TOKEN;
+const { API_TOKEN } = require('./env/config.example.js');
 
 const getRestaurantsIds = (requestObj, callback) => {
-  // use API helper here to make a request to Yelp API to grab list of 50 restaurants by id
-  // send back array of ids
-  const { term } = requestObj.term;
-  const { loc } = requestObj.loc;
+  const { term } = requestObj;
+  const { loc } = requestObj;
 
   const options = {
-    // need to incorpirate term && location into this url somehow
     url: `https://api.yelp.com/v3/businesses/search?term=${term}&location=${loc}`,
     headers: {
       'User-Agent': 'request',
       Authorization: API_TOKEN,
     },
   };
-  axios.get(options)
-    .then((response) => {
-      console.log(response.business);
-      callback(response.businesses);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  request.get(options, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      callback(JSON.parse(body.business));
+    }
+  });
 };
 
 const getRestaurantDetails = (restaurantId, callback) => {
-  // use another api helper here to make a request to API to grab details of selected restaurant
-  // send back restaurant data
   const options = {
     url: `https://api.yelp.com/v3/businesses/${restaurantId}`,
     headers: {
@@ -38,14 +33,14 @@ const getRestaurantDetails = (restaurantId, callback) => {
       Authorization: API_TOKEN,
     },
   };
-  axios.get(options)
-    .then((restaurant) => {
-      console.log(restaurant);
-      callback(restaurant);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  request.get(options, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(body);
+      callback(JSON.parse(body));
+    }
+  });
 };
 
 module.exports = {
--- server/env/config.example.js
+++ server/env/config.example.js
@@ -1,4 +1,4 @@
 module.exports = {
-API_KEY: 'ENTER API KEY',
-API_TOKEN: 'ENTER TOKEN'
+  API_KEY: 'gKPrJPaKyzvmkacQvhrozQ',
+  API_TOKEN: 'YTl6Wm-0stpU35hXMsQ3Vgq8Ryz0bXwC05a1UnEZpWXcVdi5rUgvZceURzfW_TlRCTuCHa1aqgaJd2_B36kAzlczzv6tt9uelElqY0HgmLt8P87BOm1IzwT0PokNW3Yx',
 };
