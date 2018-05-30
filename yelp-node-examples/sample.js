860f129cd66cdc732e2780fb2497e0893c5106a0
Nicholas Boylan <nboylan1pc@gmail.com>
2018-05-30 19:45:01 -0400

    helpers complete and tested

--- yelp-node-examples/sample.js
+++ yelp-node-examples/sample.js
@@ -4,7 +4,7 @@ const yelp = require('yelp-fusion');
 
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-const apiKey = '<YOUR_API_KEY>';
+const apiKey = 'YTl6Wm-0stpU35hXMsQ3Vgq8Ryz0bXwC05a1UnEZpWXcVdi5rUgvZceURzfW_TlRCTuCHa1aqgaJd2_B36kAzlczzv6tt9uelElqY0HgmLt8P87BOm1IzwT0PokNW3Yx';
 
 const searchRequest = {
   term:'Four Barrel Coffee',
