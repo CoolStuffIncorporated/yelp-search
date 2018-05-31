07a925a293164d7154dbda467eb5c7e3534dd79a
Nicholas Boylan <nboylan1pc@gmail.com>
2018-05-31 11:15:49 -0400

    completed faves bp

--- client/src/Components/Favorites.jsx
+++ client/src/Components/Favorites.jsx
@@ -3,24 +3,40 @@ import React, { Component } from 'react';
 class Favorites extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-       // no need for state, already have favorites coming in as props
-       // just refer to faves in parent for current bizes
-    // }
+    this.state = {
+    //    no need for state, already have favorites coming in as props
+    //    just refer to faves in parent for current bizes
+    }
     // method binding here any methods needed?
-            // to mouseover to show url
+      // to mouseover to show url
+      this.onHover = this.onHover.bind(this);
   }
+
+//   onHover () {
+//     console.log('on the url');
+
+//   }
  
   render () {
     return (
-      <ul className="favorites_all">
+      <ul className="favorites-all">
           {/*loop through all fave restaurants, render to page with approp. props from DB*/}
-        {this.props.restaurants.map((favorite) => {
+          {/*can add a mouseEnter event if so desired, right now url dispalying with contact info*/}
+        {this.props.favorites.map((favorite) => {
+            console.log(favorite);
             return ( 
-              <li className="favorite_item">
-                <img src={}/>
-                <div className="favorite_description">
-                  <h4>{favorite.title}</h4>
+              <li className="favorite-item">
+                <img src={favorite.image_url}/>
+                <div className="favorite-description">
+                  <h4>{favorite.name}</h4>
+                  <section className="restaurant-details">
+                    <div className="restaurant-contact-info">
+                      <span>Phone: {favorite.display_phone}</span>
+                      <span>Location: {favorite.location}</span>
+                      <span>url {favorite.url}</span>
+                    </div>
+                  </section>
+                </div>
               </li>
             )
         })}
@@ -31,15 +47,4 @@ class Favorites extends Component {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-export default Favorites
+export default Favorites;
