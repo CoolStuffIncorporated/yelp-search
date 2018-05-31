9ae633b6f211115015edbf15458f1c3bc7ccf9ca
Nicholas Boylan <nboylan1pc@gmail.com>
2018-05-30 20:31:06 -0400

    need to work through proper data

--- server/apiHelpers.js
+++ server/apiHelpers.js
@@ -4,20 +4,22 @@ const yelp = require('yelp-fusion');
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { API_TOKEN } = require('./env/config.example.js');
+// const { API_TOKEN } = require('./env/config.example.js');
 
-const apiKey = API_TOKEN;
+// const apiKey = API_TOKEN;
+const apiKey = 'YTl6Wm-0stpU35hXMsQ3Vgq8Ryz0bXwC05a1UnEZpWXcVdi5rUgvZceURzfW_TlRCTuCHa1aqgaJd2_B36kAzlczzv6tt9uelElqY0HgmLt8P87BOm1IzwT0PokNW3Yx';
 const client = yelp.client(apiKey);
 
 const getRestaurantsIds = (searchObj, callback) => {
 
   const searchRequest = {
-    term : searchObj.term,
+    // term : searchObj.term,
     loc : searchObj.loc,
   };
 
   client.search(searchRequest).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
+    // need to get all business, and use proper search term
+    const firstResult = response.jsonBody.businesses;
     const prettyJson = JSON.stringify(firstResult, null, 4);
     console.log(prettyJson);
   }).catch(e => {
@@ -28,14 +30,12 @@ const getRestaurantsIds = (searchObj, callback) => {
 
 const getRestaurantDetails = (restaurantIdObj, callback) => {
 
+  // need to modify this to get the business details somehow
   client.search(restaurantIdObj).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
-    const prettyJson = JSON.stringify(firstResult, null, 4);
-    console.log(prettyJson);
+    console.log(response.jsonBody.businesses);
   }).catch(e => {
     console.log(e);
   });
-
 };
 
 module.exports = {
@@ -46,11 +46,22 @@ module.exports = {
 
 // THIS WAS THE TESTING for ABOVE FUNCTIONS
 
-// const searchRequest = {
-//   restaurantId: 'g2BZOLMwHViRf9hsQfKm_w',
-//   location: 94102,
+const searchRequest = {
+  term: 'tacos',
+  loc : 94203,
+};
+
+getRestaurantsIds(searchRequest, (returnResponse) => {
+  console.log('in the test', returnResponse);
+});
+
+
+// const searchRequest2 = {
+//   term:'Four Barrel Coffee',
+//   loc : 'san francisco, ca',
+ //  loc : 94203,
 // };
 
-// getRestaurantDetails(searchRequest, (returnResponse) => {
+// getRestaurantDetails(searchRequest2, (returnResponse) => {
 //   console.log('in the test', returnResponse);
 // });
