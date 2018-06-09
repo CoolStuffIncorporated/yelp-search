325be58aa95a555a723b190a6c7aee7e497b875e
Rose <thebarbreaker@gmail.com>
2018-06-09 17:45:52 -0400

    fix food wall bug

--- client/dist/canvas.js
+++ client/dist/canvas.js
@@ -1,6 +1,5 @@
 let edgeLeft = 0, edgeRight = window.innerWidth, edgeTop = 0, edgeBottom = window.innerHeight;
 
-
 const canvas = document.querySelector('canvas');
 canvas.width = edgeRight;
 canvas.height = edgeBottom;
@@ -10,30 +9,6 @@ canvas.height = edgeBottom;
 console.log('canvas connected!');
 const c = canvas.getContext('2d');
 
-// let bacon = new Image;
-// bacon.onload = () => {
-//   c.drawImage(bacon,100,100, 60, 60);
-// };
-// bacon.src = 'assets/food_icons/bacon.png';
-
-///////////
-
-// class Food {
-//   constructor(width, height, imageUrl, x, y) {
-//     super();
-//     this.width = width;
-//     this.height = height;
-//     this.image = new Image();
-//     this.image.onload = () => c.drawImage(this.image,100,500, 60, 60);
-//     this.image.src = imageUrl;
-//   }
-//   update() {
-
-//   }
-// }
-
-
-
 function init() {
   const randV = () => {
     let v = Math.random() * 10 + 5;
@@ -41,15 +16,13 @@ function init() {
   }
   const randX = () => Math.random() * canvas.width;
   const randY = () => Math.random() * 200 - 200;
-  const foodMaker = foods => {
-    return foods.map(food => new Food(`${food}`, `assets/food_icons/${food}.png`, 80, 80, randX(), randY(), randV(), randV()))
-  }
-  let foodItems = foodMaker(['bacon', 'pizza', 'hamburger', 'hotdog', 'taco', 'mushroom', 'milkshake', 'toast', 'cookie', 'ketchup']);
+  const foodMaker = foods => foods.map(food => new Food(`${food}`, `assets/food_icons/${food}.png`, 80, 80, randX(), randY(), randV(), randV()));
+  let foodItems = foodMaker(['bacon', 'pizza', 'hamburger', 'hotdog', 'taco', 'mushroom', 'milkshake', 'toast', 'cookie', 'ketchup', 'radish', 'donut', 'onigiri', 'soda', 'scallion', 'cupcake', 'croissant']);
   setInterval(() => {
     c.clearRect(0, 0, canvas.width, canvas.height);
     foodItems.forEach(item => item.draw());
-    let bacon = new Image;bacon.onload = () => c.drawImage(bacon,610,50, 60, 60);
-    bacon.src = 'assets/food_icons/bacon.png'; // test bacon
+    // let bacon = new Image;bacon.onload = () => c.drawImage(bacon,610,50, 60, 60);
+    // bacon.src = 'assets/food_icons/bacon.png'; // test bacon
   }, 20);
 }
 
@@ -62,8 +35,7 @@ function drawImageRot(img, x, y, width, height, deg){
   c.translate(-(x + width / 2), -(y + height / 2));
 }
 
-let dampening = .9;
-let shrinkage = .9;
+let dampening = .9, shrinkage = .9;
 
 function Food(name, imgSrc, width, height, x, y, dx, dy) {
   this.name = name;
@@ -80,7 +52,6 @@ function Food(name, imgSrc, width, height, x, y, dx, dy) {
   this.torque = .05;
   this.image.onload = () => this.draw();
   this.draw = function(deg = 0) {
-    // c.clearRect(0, 0, canvas.width, canvas.height);
     if (this.height < 30) return;
     drawImageRot(this.image, this.x, this.y, this.width, this.height, this.deg);
     this.dy += this.gravity;
@@ -89,7 +60,7 @@ function Food(name, imgSrc, width, height, x, y, dx, dy) {
       this.height *= shrinkage;
       this.width *= shrinkage;
     }
-    if (this.x + this.width > canvas.width || this.x < -this.width) {
+    if (this.x + this.width > canvas.width || this.x < 0) {
       this.dx = -this.dx * dampening;
       this.height *= shrinkage;
       this.width *= shrinkage;
@@ -103,6 +74,6 @@ function Food(name, imgSrc, width, height, x, y, dx, dy) {
 
 }
 
-init();
+// init();
 
-// setTimeout(init, 5000);
+setTimeout(init, 3000);
