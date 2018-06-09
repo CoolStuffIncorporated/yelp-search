5601d4c48e8ebb04bcecfb6460c1b1c262723def
Charles <charles.hufnagel@protonmail.com>
2018-06-08 20:01:36 -0400

    Suggestions, auto-complete, and Material-UI added to food type selector

--- client/src/Components/Search.jsx
+++ client/src/Components/Search.jsx
@@ -56,15 +56,17 @@ function renderSuggestion(suggestion, { query, isHighlighted }) {
   return (
     <MenuItem selected={isHighlighted} component="div">
       <div>
-        {parts.map((part, index) => part.highlight ? (
-            <span key={String(index)} style={{ fontWeight: 300 } }>
+        {parts.map((part, index) => {
+          return part.highlight ? (
+            <span key={String(index)} style={{ fontWeight: 300 }}>
               {part.text}
             </span>
           ) : (
             <strong key={String(index)} style={{ fontWeight: 500 }}>
               {part.text}
             </strong>
-          ))}
+          );
+        })}
       </div>
     </MenuItem>
   );
@@ -89,17 +91,19 @@ function getSuggestions(value) {
   const inputLength = inputValue.length;
   let count = 0;
 
-  return inputLength === 0 ? []
+  return inputLength === 0
+    ? []
     : suggestions.filter(suggestion => {
-      const keep = count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
+      const keep =
+          count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
 
       if (keep) {
         count += 1;
       }
+
       return keep;
     });
 }
-
 
 const styles = theme => ({
   container: {
@@ -117,7 +121,7 @@ const styles = theme => ({
   suggestion: {
     display: 'block',
   },
-  suggestionList: {
+  suggestionsList: {
     margin: 0,
     padding: 0,
     listStyleType: 'none',
@@ -133,7 +137,6 @@ class Search extends React.Component {
     };
     this.inputZip = this.inputZip.bind(this);
     this.checkEnter = this.checkEnter.bind(this);
-    // this.inputFood = this.inputFood.bind(this);
   }
   inputZip(e) {
     this.setState({ zip: e.target.value });
@@ -145,24 +148,20 @@ class Search extends React.Component {
       this.props.getRestaurants(this.state.value, this.state.zip);
     }
   }
-  // modify
-  // inputFood(e) {
-  //   this.setState({ foodType: e.target.value });
-  // }
 
-  onChange = (event, { newValue }) => {
+  handleChange = (event, { newValue }) => {
     this.setState({
       value: newValue,
     });
   };
 
-  onSuggestionsFetchRequested = ({ value }) => {
+  handleSuggestionsFetchRequested = ({ value }) => {
     this.setState({
       suggestions: getSuggestions(value),
     });
   };
 
-  onSuggestionsClearRequested = () => {
+  handleSuggestionsClearRequested = () => {
     this.setState({
       suggestions: [],
     });
@@ -171,52 +170,35 @@ class Search extends React.Component {
   render() {
     // const { value, suggestions } = this.state;
     const { classes } = this.props;
-    // const inputProps = {
-
-    // };
-
-    // const zipProps = {
-    //   id: 'zip',
-    //   placeholder: "What's your zip code?",
-    //   type: 'number',
-    //   maxLength: '5',
-    //   min: '10000',
-    //   max: '99999',
-    //   onChange: this.inputZip,
-    //   onKeyUp: this.checkEnter,
-    // };
 
     const imgPath = './assets/yelp_logo/yelp_fullcolor_outline.png';
 
     return (
       <div className="search">
-        <img className="yelp-logo" src={imgPath} />
-        <div className="search-fields">
-        <Autosuggest
-          theme={{
-            container: classes.container,
-            suggestionsContainerOpen: classes.suggestionsContainerOpen,
-            suggestionsList: classes.suggestionList,
-            suggestion: classes.suggestion,
-          }}
-          renderInputComponent={renderInput}
-          suggestions={this.state.suggestions}
-          onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
-          renderSuggestionsContainer={renderSuggestionsContainer}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={{
-            classes,
-            placeholder: 'What kind of food are you in the mood for?',
-            value: this.state.value,
-            onChange: this.onChange,
-          }}
-        />
-      );
-    }
-  }
-        <input
+      <img className="yelp-logo" src={imgPath} />
+      <div className="search-fields">
+      <Autosuggest
+        theme={{
+          container: classes.container,
+          suggestionsContainerOpen: classes.suggestionsContainerOpen,
+          suggestionsList: classes.suggestionsList,
+          suggestion: classes.suggestion,
+        }}
+        renderInputComponent={renderInput}
+        suggestions={this.state.suggestions}
+        onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
+        renderSuggestionsContainer={renderSuggestionsContainer}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={{
+          classes,
+          placeholder: 'Search a country (start with a)',
+          value: this.state.value,
+          onChange: this.handleChange,
+        }}
+      />
+      <input
           id="zip"
           placeholder="What's your zip code?"
           type="number"
@@ -226,11 +208,12 @@ class Search extends React.Component {
           onChange={this.inputZip}
           onKeyUp={this.checkEnter}
           />
-        </div>
+      </div>
       </div>
     );
   }
 }
+
 
 Search.propTypes = {
   classes: PropTypes.object.isRequired,
