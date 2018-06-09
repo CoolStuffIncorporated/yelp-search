3e9729e3bcd7f55f358881e983281828c0ef932c
Rose <thebarbreaker@gmail.com>
2018-06-09 15:40:54 -0400

    created bouncy foods

--- client/dist/canvas.js
+++ client/dist/canvas.js
@@ -32,22 +32,33 @@ const c = canvas.getContext('2d');
 //   }
 // }
 
-// let bacon = new Food(60, 60, 'assets/food_icons/bacon.png', 100, 500);
+
+
 function init() {
-  let bacon = new Food('bacon', 'assets/food_icons/bacon.png', 60, 60, 100, 200, 1, 1);
-  let pizza = new Food('pizza', 'assets/food_icons/pizza.png', 60, 60, 300, 200, 1, 1);
-  setInterval(() => bacon.draw(2), 100);
-  // bacon.draw();
+  const randV = () => Math.random() * 10 + 5;
+  console.log(canvas.width);
+  const randX = () => Math.random() * canvas.width;
+  const randY = () => Math.random() * canvas.height;
+  // const foodMaker = 
+  let bacon = new Food('bacon', 'assets/food_icons/bacon.png', 80, 80, randX(), randY(), randV(), randV());
+  let pizza = new Food('pizza', 'assets/food_icons/pizza.png', 80, 80, randX(), randY(), randV(), randV());
+  setInterval(() => {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    bacon.draw();
+    pizza.draw();
+  }, 20);
 }
 
-function drawImageRot(img,x,y,width,height,deg){
-  var rad = deg * Math.PI / 180;   //Convert degrees to radian 
-  ctx.translate(x + width / 2, y + height / 2);  //Set the origin to the center of the image
-  ctx.rotate(rad);   //Rotate the canvas around the origin  
-  ctx.drawImage(img, width/2*(-1), height/2*(-1),width,height); //draw the image  
-  ctx.rotate(rad * ( -1 ) );   //reset the canvas  
-  ctx.translate((x + width / 2) * (-1), (y + height / 2) * (-1));
+function drawImageRot(img, x, y, width, height, deg){
+  var rad = deg * Math.PI / 180;
+  c.translate(x + width / 2, y + height / 2);
+  c.rotate(rad);
+  c.drawImage(img, -width/2, -height/2, width, height);
+  c.rotate(-rad);
+  c.translate(-(x + width / 2), -(y + height / 2));
 }
+
+let dampening = .9;
 
 function Food(name, imgSrc, width, height, x, y, dx, dy) {
   this.name = name;
@@ -59,15 +70,29 @@ function Food(name, imgSrc, width, height, x, y, dx, dy) {
   this.dy = dy;
   this.x = x;
   this.y = y;
+  this.gravity = 1;
+  this.deg = 0;
+  this.torque = .05;
   this.image.onload = () => this.draw();
   this.draw = function(deg = 0) {
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    c.rotate(deg);
-    c.drawImage(this.image, this.x, this.y, this.width, this.height);
-    c.rotate(-deg);
+    // c.clearRect(0, 0, canvas.width, canvas.height);
+    if (this.height < 30) return;
+    drawImageRot(this.image, this.x, this.y, this.width, this.height, this.deg);
+    this.dy += this.gravity;
+    if (this.y + this.height > canvas.height) {
+      this.dy = -this.dy * dampening;
+      this.height *= .9;
+      this.width *= .9;
+    }
+    if (this.x + this.width > canvas.width || this.x < -this.width) {
+      this.dx = -this.dx * dampening;
+      this.height *= .9;
+      this.width *= .9;
+    }
+    this.x += this.dx;
     this.y += this.dy;
-    console.log('this y', this.name, this.y);
-    // setInterval(() => this.draw(deg++), 1000);
+    this.torque = this.torque > 0.5 ? this.torque: this.torque + 0.1;
+    this.deg += this.torque;
   }
   this.draw = this.draw.bind(this);
 
