5e905150310f8404823cd1364371b658bfc36571
Charles <charles.hufnagel@protonmail.com>
2018-06-11 03:36:06 -0400

    Add service worker, change webpack entry point, add index.js container page. Dockerized site launched and linked to master branch

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/App.jsx`,
+  entry: `${SRC_DIR}/index.js`,
   mode: 'production',
   output: {
     filename: 'bundle.js',
